d82c36504243d59c660060672345e2fb36dd0920
zahur <zahurmeerun@hotmail.com>
2022-04-28 10:56:01 +0400

    add: home page app with basic settings

--- heatmap/settings.py
+++ heatmap/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -122,7 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+# For main static file not tied up to app and in base_dir/Required for base.css so django will look here too
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+# This will add the file to the media folder and not to another url
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
